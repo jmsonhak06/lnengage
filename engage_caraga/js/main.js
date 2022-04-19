a2554daba6c7fc483d71656b13c65018a7ceaf2c
jmsonhak06 <sonakatsukihak@gmail.com>
2022-04-19 19:28:57 +0800

    Deleted past commits

---
+++ engage_caraga/js/main.js
@@ -0,0 +1,20 @@
+(function($) {
+
+	"use strict";
+
+    const fullHeight = function () {
+
+        $('.js-fullheight').css('height', $(window).height());
+        $(window).resize(function () {
+            $('.js-fullheight').css('height', $(window).height());
+        });
+
+    };
+    fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
+
+})(jQuery);
+
